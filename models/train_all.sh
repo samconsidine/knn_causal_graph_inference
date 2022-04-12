161c10b9fb1282042bab3b079a3ffb6018447209
Sam Considine <sambconsidine@gmail.com>
2022-04-12 12:50:01 +0100

    Added dummy datagen

---
+++ models/train_all.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Running training procedure over the following models:"
+echo "-----------------------------------------------------"
+echo "1. DGCNN - Dynamic Graph CNN for Learning on Point Clouds"
+echo "2. DGM - Differentiable Graph Module (DGM for Graph Convolutional Networks)"
+echo "3. SLAPS-GNN - Self-Supervision Improves Structure Learning for Graph Neural Networks"
+echo ""
